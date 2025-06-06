3364bb2cf1a9d601dc6f58ad3feb99284adc9d18
anderskambenders <a.zetsmanis@mail.ru>
2025-06-07 00:24:46 +0300

    feat: add prisma, add api/yaml

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import * as YAML from 'yamljs';
+import * as swaggerUi from 'swagger-ui-express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   const port = parseInt(process.env.PORT) || 4000;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
