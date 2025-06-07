266cb45ed12d56595da67f94ddda0ab071030b4a
anderskambenders <a.zetsmanis@mail.ru>
2025-06-08 00:58:31 +0300

    feat: add prisma module

--- src/db/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataService } from './database.service';
-
-@Module({
-  providers: [DataService],
-  exports: [DataService],
-})
-export class DataModule {}
