5f3b10f7b55eda5a235afa867ba3c296d01fa83f
anderskambenders <a.zetsmanis@mail.ru>
2025-06-09 00:15:33 +0300

    feat: add favs to prisma

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:22.16
+FROM node:20.11-alpine
 WORKDIR /app
 COPY . .
-RUN npm ci && npm cache clean --force
+RUN npm ci --legacy-peer-deps && npm cache clean --force
 EXPOSE 4000
 CMD ["npm", "run", "start:migration:dev"]
--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -4,11 +4,11 @@ import FavoritesController from './favorites.controller';
 import TracksService from '../tracks/tracks.service';
 import AlbumsService from '../albums/albums.service';
 import ArtistsService from '../artists/artists.service';
-import { DataModule } from '../db/database.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [FavoritesController],
   providers: [FavoritesService, TracksService, AlbumsService, ArtistsService],
-  imports: [DataModule],
+  imports: [PrismaModule],
 })
 export class FavoritesModule {}
