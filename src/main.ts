5c7892bf3861c4e65a1ba83354d603ebb0e7d693
anderskambenders <a.zetsmanis@mail.ru>
2025-06-15 23:16:36 +0300

    fix: fix env and main

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,21 @@ import { NestFactory } from '@nestjs/core';
 import * as YAML from 'yamljs';
 import * as swaggerUi from 'swagger-ui-express';
 import * as path from 'path';
+import LoggingService from './logging/logging.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggingService(),
+  });
+  const logger = app.get(LoggingService);
+  app.useLogger(logger);
+  process.on('uncaughtException', (err) => {
+    logger.error(`Caught Exception - ${err.message}`);
+    process.exit(1);
+  });
+  process.on('unhandledRejection', (reason) => {
+    logger.warn(`Rejection at promise - ${reason}`);
+  });
   const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   const port = parseInt(process.env.PORT) || 4000;
